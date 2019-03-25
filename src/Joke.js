76914361d8e3f7c1361affac8229c93dd6c97af3
annika <annikaslund@gmail.com>
2019-03-25 15:54:38 -0700

    Add up click and down click functionality

--- src/Joke.js
+++ src/Joke.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 
 class Joke extends Component {
-    
-
-    
-    shouldComponentUpdate(nextProps, nextState) {
+    // shouldComponentUpdate(nextProps, nextState) {
         
+    // }
+    render(){
+        return (
+            <div className="Joke">
+                {this.props.jokeText}
+                <button onClick={this.props.handleUpClick}>Up</button>
+                {this.props.total}
+                <button onClick={this.props.handleDownClick}>Down</button>
+            </div>
+        );
     }
-    
 }
 
 export default Joke;
--- src/JokeList.js
+++ src/JokeList.js
@@ -8,31 +8,53 @@ class JokeList extends Component {
         this.state = {
             jokes: [] //{joke: "contents", id: "", upCount: #, downCount: #}
         } 
-        //this.voteUp = this.voteUp.bind(this);
+
+        this.voteUp = this.voteUp.bind(this);
         //this.voteDown = this.voteDown.bind(this);
     }
 
-    //handles joke up vote click from child (Joke)
-    // voteUp(){
+    // handles joke up vote click from child (Joke)
+    voteUp(id){
+        const updateJokeCount = this.state.jokes.map( joke => {
+            if (joke.id === id){
+                return {...joke, upCount: joke.upCount + 1}
+            }
+            return joke;
+        })
 
-    // }
+        this.setState({ jokes: updateJokeCount })
+    }
 
     //handles joke down vote click from child (Joke)
-    // voteDown(){
+    voteDown(id){
+        const updateJokeCount = this.state.jokes.map( joke => {
+            if (joke.id === id){
+                return {...joke, downCount: joke.downCount - 1}
+            }
+            return joke;
+        })
 
-    // }
+        this.setState({ jokes: updateJokeCount })
+    }
 
     // runs after first render 
     // makes API calls to dadjokez
     async componentDidMount() {
-        const URL = "https://icanhazdadjoke.com/search?limit=10";
+        const URL = "https://icanhazdadjoke.com/";
         const header = {headers: {"Accept": "application/json"}};
-        let response = await axios.get(URL, header);
-        let jokes = response.data.results;
-        let newJokes = jokes.map(joke => {
-            return {...joke, upCount: 0, downCount: 0};
-        })
 
+        let responses = [];
+
+        for (let i = 0; i < 10; i++){
+            let response = axios.get(URL, header);
+            responses.push(response);
+        }
+
+        let jokes = await Promise.all(responses);
+        let newJokes = jokes.map(joke => {
+            return {...joke.data, upCount: 0, downCount: 0};
+        })
+        
         this.setState({
            jokes: newJokes
         })
@@ -41,7 +63,7 @@ class JokeList extends Component {
     render() {
         return (
             <div className="JokeList">
-                
+                {this.state.jokes.map(joke => <Joke jokeText={joke.joke} total={joke.upCount + joke.downCount} handleUpClick={() => this.voteUp(joke.id)} handleDownClick={() => this.voteDown(joke.id)}/>)}
             </div>
         )
     }
